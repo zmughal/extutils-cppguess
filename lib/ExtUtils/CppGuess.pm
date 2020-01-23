dadfa08a26e0587c16def78898fa9cf6efa2b7e5
Ed J <mohawk2@users.noreply.github.com>
2020-01-23 15:25:06 +0000

    0.21

--- lib/ExtUtils/CppGuess.pm
+++ lib/ExtUtils/CppGuess.pm
@@ -172,7 +172,7 @@ use Capture::Tiny 'capture_merged';
 use File::Spec::Functions qw(catfile);
 use File::Temp qw(tempdir);
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 sub new {
     my( $class, %args ) = @_;
